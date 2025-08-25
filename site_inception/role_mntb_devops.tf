8f980ae05bf26202550fc231bfc829362c0c1cf1
Lucas Cañero <54242962+lucascanero@users.noreply.github.com>
2025-08-25 18:11:53 +0100

    Create role_mntb_devops.tf

---
+++ site_inception/role_mntb_devops.tf
@@ -0,0 +1,38 @@
+# Policy to MontBlu users
+resource "aws_iam_policy" "mntb" {
+  description = "Managed by Terraform"
+  name        = "${local.resource_name_prefix}_devops_mntb"
+  policy      = data.aws_iam_policy_document.ops.json
+}
+
+# Role to MontBlu users
+resource "aws_iam_role" "mntb" {
+  name               = "${local.resource_name_prefix}-devops-mntb"
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "AWS": "arn:aws:iam::101998516976:root"
+      },
+      "Action": "sts:AssumeRole",
+      "Condition": {}
+    }
+  ]
+}
+EOF
+}
+
+# Required Permissions
+resource "aws_iam_role_policy_attachment" "mntb_policy" {
+  role       = aws_iam_role.mntb.name
+  policy_arn = aws_iam_policy.mntb.arn
+}
+
+# DynamoDB Full Access
+resource "aws_iam_role_policy_attachment" "mntb_dynamodb" {
+  role       = aws_iam_role.mntb.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess"
+}
